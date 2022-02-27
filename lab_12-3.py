670d341fbc93d27c736839374f47802e03117a0f
P22srogers <p22srogers@fairfieldprep.org>
2022-02-27 08:57:02 -0500

    Done

---
+++ lab_12-3.py
@@ -0,0 +1,9 @@
+# Author: SMR (AMDG) 2/16/22
+# Setting variable of weekdays
+weekdays=("Monday","Tuesday","Wednesday","Thursday","Friday")
+# Setting variable of weekends
+weekends=("Saturday","Sunday")
+# # adding both together in order to get all days of the week
+days_of_the_week= weekdays + weekends
+# finally printing out all of the days
+print(days_of_the_week)
---
+++ lab_12-4.py
@@ -0,0 +1,7 @@
+# Author: SMR (AMDG) 2/16/22
+# First created a variable set as a tuple.
+day_date = ("16","17","18","19","20")
+# Then created a list of classes to assign to set numbers
+(stats, calc, english, comp_sci, theology) = day_date
+# After assigning the numbers, prints out dates.
+print(stats, calc, english, comp_sci, theology)
